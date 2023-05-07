227475e14d8ba73e006362e595520f6f64a1105c
Timur <tim.id.campus@gmail.com>
2023-05-07 17:11:50 +0200

    CarController endpoints adjusted to fit the Part I & pom for kafka extended

--- src/main/java/com/separttwo/carmanagementmicroservice/CarController.java
+++ src/main/java/com/separttwo/carmanagementmicroservice/CarController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cars")
+@RequestMapping("/car-rental/api/v1/cars")
 public class CarController {
     @Autowired
     private CarService carService;
